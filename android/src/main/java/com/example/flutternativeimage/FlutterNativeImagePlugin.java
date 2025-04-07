3916692392b0e07b663b0edbbb39ca5cb26bca08
Batrareth Sudprasert <bsuprase@sodality.co.th>
2025-04-07 16:28:39 +0700

    Embedding Plugin V2

--- android/src/main/java/com/example/flutternativeimage/FlutterNativeImagePlugin.java
+++ android/src/main/java/com/example/flutternativeimage/FlutterNativeImagePlugin.java
@@ -13,32 +13,44 @@ import io.flutter.plugin.common.PluginRegistry;
 public class FlutterNativeImagePlugin implements FlutterPlugin {
   private static final String CHANNEL_NAME = "flutter_native_image";
   private MethodChannel channel;
+  private Context context; // Store context if MethodCallHandlerImpl needs it after setup
+  
   /**
    * Plugin registration.
    */
-  public static void registerWith(PluginRegistry.Registrar registrar) {
-    final FlutterNativeImagePlugin plugin = new FlutterNativeImagePlugin();
-    plugin.setupChannel(registrar.messenger(), registrar.context());
+  // public static void registerWith(PluginRegistry.Registrar registrar) {
+  //   final FlutterNativeImagePlugin plugin = new FlutterNativeImagePlugin();
+  //   plugin.setupChannel(registrar.messenger(), registrar.context());
+  // }
+
+  @Override
+  public void onAttachedToEngine(@NonNull FlutterPlugin.FlutterPluginBinding binding) {
+      // Get the application context
+      this.context = binding.getApplicationContext();
+      // Get the BinaryMessenger
+      BinaryMessenger messenger = binding.getBinaryMessenger(); // Or binding.getFlutterEngine().getDartExecutor()
+      // Setup the channel using the v2 binding
+      setupChannel(messenger, this.context);
   }
 
   @Override
-  public void onAttachedToEngine(FlutterPlugin.FlutterPluginBinding binding) {
-    setupChannel(binding.getFlutterEngine().getDartExecutor(), binding.getApplicationContext());
-  }
-
-  @Override
-  public void onDetachedFromEngine(FlutterPlugin.FlutterPluginBinding binding) {
+  public void onDetachedFromEngine(@NonNull FlutterPlugin.FlutterPluginBinding binding) {
+    // Clean up resources
     teardownChannel();
-  }
+    this.context = null; // Release context
+}
 
   private void setupChannel(BinaryMessenger messenger, Context context) {
     channel = new MethodChannel(messenger, CHANNEL_NAME);
+    // Assuming MethodCallHandlerImpl needs the context
     MethodCallHandlerImpl handler = new MethodCallHandlerImpl(context);
     channel.setMethodCallHandler(handler);
-  }
+}
 
   private void teardownChannel() {
-    channel.setMethodCallHandler(null);
-    channel = null;
-  }
+    if (channel != null) {
+        channel.setMethodCallHandler(null);
+        channel = null;
+    }
+}
 }
